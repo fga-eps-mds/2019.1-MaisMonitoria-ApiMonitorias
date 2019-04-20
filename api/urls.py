4586d0084ae2f363404b14172591e755e770b7c6
João Soares <jpcirqueira81@gmail.com>
2019-04-20 17:03:09 -0300

    [#TS07] Criado classe areas de interesse.
    Signed-off-by: João Soares <jpcirqueira81@gmail.com>
    
    Co-authored-by: MoacirMSJ <moacirmascarenha86@gmail.com>
    Co-authored-by: RCristyan <rcristyan9@gmail.com>

--- api/urls.py
+++ api/urls.py
@@ -3,11 +3,13 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework import routers
 from user_account.views import UserAccountViewset
+from user_account.views import InterestAreaViewset
 from tutoring_session.views import TutoringSessionViewset
 
 router = routers.DefaultRouter()
 router.register(r'user', UserAccountViewset)
 router.register(r'tutoring', TutoringSessionViewset)
+router.register(r'interestarea',  InterestAreaViewset)
 
 urlpatterns = [
     path('', include(router.urls)),
