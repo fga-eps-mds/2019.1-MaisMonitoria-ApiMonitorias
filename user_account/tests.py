61bc2c3a59740af5102d8767930243408d47ac50
João Soares <jpcirqueira81@gmail.com>
2019-04-25 00:49:06 -0300

    [REFACTOR] Criado teste da model UserAccount.
    Signed-off-by: João Soares <jpcirqueira81@gmail.com>
    Co-authored-by: MatheusEstanislau <matheus.estanislau7@gmail.com>
    Co-authored-by: MoacirMSJ <moacirmascarenha86@gmail.com>
    Co-authored-by: lukassxp <lpalucas.10@gmail.com>

--- user_account/tests.py
+++ user_account/tests.py
@@ -1,13 +1,14 @@
 from rest_framework import status
 from rest_framework.test import APITestCase
 from .models import InterestArea
+from .models import UserAccount
 
 # Create your tests here.
 
-class AccountTests(APITestCase):
+class InterestAreaTests(APITestCase):
     def setUp(self):
         self.valid_payload = {
-            'name': 'Fisica'
+            'name': 'teste'
         }
 
         self.invalid_payload = {
@@ -27,3 +28,59 @@ class AccountTests(APITestCase):
         data = self.invalid_payload
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class UserAccountTests(APITestCase):
+    def setUp(self):
+        self.valid_payload = {
+            'user_account_id': '123',
+            'name': 'teste',
+            'email': 'teste@gmail.com',
+            'course': 'ENGENHARIAS'
+        }
+
+        self.invalid_payload = {
+            'user_account_id': '',
+            'name': '',
+            'email': '',
+            'course': ''
+        }
+        self.database_register = {
+            "user_account_id": "123",
+            "name": "teste",
+            "email": "teste@gmail.com",
+            "registration_date": "2019-04-25T03:22:44.599483Z",
+            "description": "",
+            "course": "ENGENHARIAS",
+            "account_state": True,
+            "photo_url": "",
+            "interest_areas": [],
+            "monitoring": [],
+            "monitoring_history": []
+        }
+
+    def test_create_valid_user(self):
+        url = '/user/'
+        data = self.valid_payload
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual( UserAccount.objects.count(), 1)
+        self.assertEqual(UserAccount.objects.get().user_account_id, self.database_register['user_account_id'])
+        self.assertEqual(UserAccount.objects.get().name, self.database_register['name'])
+        self.assertEqual(UserAccount.objects.get().email, self.database_register['email'])
+        # self.assertEqual(UserAccount.objects.get().registration_date, self.database_register)
+        self.assertEqual(UserAccount.objects.get().description, self.database_register['description'])
+        self.assertEqual(UserAccount.objects.get().course, self.database_register['course'])
+        self.assertEqual(UserAccount.objects.get().account_state, self.database_register['account_state'])
+        self.assertEqual(UserAccount.objects.get().photo_url, self.database_register['photo_url'])
+        # self.assertEqual(UserAccount.objects.get().interest_areas, self.database_register['interest_areas'])
+        # self.assertEqual(UserAccount.objects.get().monitoring, self.database_register["monitoring"])
+        # self.assertEqual(UserAccount.objects.get().monitoring_history, self.database_register["monitoring_history"])
+
+    def test_create_invalid_user(self):
+        url = '/user/'
+        data = self.invalid_payload
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
