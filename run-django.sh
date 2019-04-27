b4d96a03905270fef007394299327407240a6ffb
lukassxp <lpalucas.10@gmail.com>
2019-04-27 18:29:01 -0300

    [REFACTOR] Adicionado sleep para o django aguardar o postgres.

--- run-django.sh
+++ run-django.sh
@@ -1,8 +1,4 @@
+sleep 120
 python3 manage.py makemigrations 
-
-until python3 manage.py migrate; do
-  sleep 2
-  echo "Retry!";
-done
-
+python3 manage.py migrate --noinput
 python3 manage.py runserver 0.0.0.0:8001
