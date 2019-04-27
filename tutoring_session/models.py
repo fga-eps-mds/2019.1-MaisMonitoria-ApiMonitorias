2fba1b1a763d2ff59cf19539eead5a0df7477acb
lukassxp <lpalucas.10@gmail.com>
2019-04-27 13:17:05 -0300

    [REFACTOR] Ajustada forma de importar models.

--- tutoring_session/models.py
+++ tutoring_session/models.py
@@ -5,12 +5,12 @@ from user_account.models import UserAccount
 class Solicitation(models.Model):
     id_solicitation = models.AutoField(primary_key=True)
     status_solicitation = models.BooleanField(default = True)
-    resquester = models.ForeignKey(UserAccount,on_delete=models.CASCADE)
+    resquester = models.ForeignKey("user_account.UserAccount", on_delete=models.CASCADE)
     create_date = models.DateTimeField(auto_now_add = True)
 
 class TutoringSession(models.Model):
     id_tutoring_session = models.AutoField(primary_key=True)
-    monitor_id= models.ForeignKey(UserAccount,on_delete=models.CASCADE, related_name='monitor')
+    monitor_id= models.ForeignKey("user_account.UserAccount", on_delete=models.CASCADE, related_name='monitor')
     name = models.CharField(max_length=150)
     subject = models.CharField(max_length=200)
     description = models.CharField(max_length=500, default="")
