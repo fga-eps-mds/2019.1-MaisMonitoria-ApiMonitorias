8a296363a92bd055d3e8e410c2e4ed0e0caafb9e
MoacirMSJ <moacirmascarenha86@gmail.com>
2019-05-26 13:18:55 -0300

    [TS18] Criado teste test_delete_like.

--- like/tests.py
+++ like/tests.py
@@ -34,4 +34,24 @@ class LikeTests(APITestCase):
         self.assertEqual(Like.objects.count(), 1)
         self.assertEqual(Like.objects.get().id_like, 1)
 
+    def test_delete_like(self):
+        user1= UserAccount(name='moacir', user_account_id="1", 
+                           email='moacir@moacir', course='AERO')
+        user1.save()                   
+        tutoring = TutoringSession(monitor=user1,name='derivada', subject='c1')
+        tutoring.save()
+        user2=UserAccount(name='moacir', user_account_id="2", 
+                           email='moacir@moacir', course='AERO')
+        user2.save()
+        like= Like(user_that_likes=user2,tutoring_session = tutoring )
+        like.save()
+        
+        data = {'id_like':2}
+        url= '/like/'+str(data['id_like'])+'/'
+        
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Like.objects.count(), 0)
+        
+
     
